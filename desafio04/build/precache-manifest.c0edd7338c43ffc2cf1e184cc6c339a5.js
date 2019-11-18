ce3d5b700fbb0030cbee637b42ab8a8c46b8db8b
ErickMPSilva <56652881+ErickMPSilva@users.noreply.github.com>
2019-11-18 12:15:33 -0300

    Add files via upload

---
+++ desafio04/build/precache-manifest.c0edd7338c43ffc2cf1e184cc6c339a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a2cca33bc87faa4818dab309324bf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b87d9405877ed3dcdaa2",
+    "url": "/static/css/main.5049edd9.chunk.css"
+  },
+  {
+    "revision": "a2e27acb0a4963d20552",
+    "url": "/static/js/2.26406b6f.chunk.js"
+  },
+  {
+    "revision": "b87d9405877ed3dcdaa2",
+    "url": "/static/js/main.76c22fe8.chunk.js"
+  },
+  {
+    "revision": "93c86cb0d57198c6cd00",
+    "url": "/static/js/runtime-main.1882608c.js"
+  }
+]);
